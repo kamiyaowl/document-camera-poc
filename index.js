b25080079ff479ff3ae80e93f1ff45040ee4bf52
K <kamiyaowl@gmail.com>
2019-03-05 13:37:54 +0900

    get parameterでコンフィグできるようになった

--- index.js
+++ index.js
@@ -1,5 +1,20 @@
 //============================== setup ==============================
 console.log(" === sashimi === ");
+// config params
+function get_query() {
+    if(!window.location.search) return {};
+    else {
+        const dst = {};
+        const params = window.location.search.split("?")[1].split("&");
+        params.forEach(function(v, i) {
+            const values = v.split("=");
+            dst[values[0]] = values[1];
+        });
+        return dst;
+    }
+}
+const config = get_query();
+console.log('config', config);
 // const
 const sashimi_path = "./images/food_sashimi.png";
 const tampopo_path = "./images/flower_tanpopo.png";
@@ -19,6 +34,7 @@ stage.addChild(player);
 const player_center = new createjs.Shape();
 player_center.graphics.beginFill("Red").drawCircle(0,0,100);
 player_center.alpha = 0.3;
+player_center.visible = (config.show_pointer ? true : false);
 stage.addChild(player_center);
 
 createjs.Ticker.addEventListener('tick', function(){
